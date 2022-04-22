401a325b57833a767205c4ddfbf139a24ae4cc4e
Phuong Pham <pqphuong.vt04@gmail.com>
2022-04-22 12:33:59 +0100

    remove old code

--- single.php
+++ single.php
@@ -5,21 +5,9 @@
 		<div class="container">
 			<div class="grid">
 				<div class="col-12">
-
 					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php if ( get_post_status ( get_the_ID() ) == 'private' ) { ?>
-						    <div class="private-tab">
-								<p>THIS PAGE IS IN PRIVATE</p>
-							</div>
-						<?php } ?>
-					    	<h1><?php the_title(); ?></h1>
-							<h2><?php hex_posted_on(); ?> <?php hex_posted_by(); ?></h2>
-							<h3><?php hex_tags_categories(); ?></h3>
-							<?php the_content(); ?>
-
+            <h1><?php the_title(); ?>
 					<?php endwhile; ?>
-					
 				</div>
 			</div>
 		</div>
